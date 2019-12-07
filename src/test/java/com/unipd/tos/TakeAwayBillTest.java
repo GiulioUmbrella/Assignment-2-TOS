48e8b94c78154a115b595cc6cba9bbb79bcf6b04
GiulioUmbrella <giulio.umbrella@gmail.com>
2019-12-07 20:07:55 +0100

    inserita funzione setup per indipendenza test e aggiunto metodo privato per evitare duplicazioni di codice

--- src/test/java/com/unipd/tos/TakeAwayBillTest.java
+++ src/test/java/com/unipd/tos/TakeAwayBillTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.unipd.tos.business.*;
@@ -17,11 +18,27 @@ import com.unipd.tos.model.ItemType;
 import com.unipd.tos.model.MenuItemImpl;
 
 
+
 public class TakeAwayBillTest {	
 
+
+ TakeAwayBill tb = new TakeAwayBillImpl();
+ List<MenuItem> mil = new ArrayList<MenuItem>(); 
+
+ private void addElementsToMenu(ItemType item, String name, double price, int n) {
+  for(int i = 0; i < n; i++)
+  mil.add(new MenuItemImpl(item,name, price));
+ }
+  
+ @Before 
+ public void setUp() {
+  mil = new ArrayList<MenuItem>();
+  tb = new TakeAwayBillImpl();
+ }
+ 
+	
  @Test
 public void testNullMenu() {
- TakeAwayBill tb = new TakeAwayBillImpl();
  try {
  assertEquals(0, tb.getOrderPrice(null) ,0);
  } catch (TakeAwayBillException e) {
@@ -32,22 +49,22 @@ public void testNullMenu() {
  
 @Test 
 public void testEmptyMenu() throws TakeAwayBillException {
- TakeAwayBill tb = new TakeAwayBillImpl();
- List<MenuItem> mil = new ArrayList<MenuItem>();
  assertEquals(0.0,tb.getOrderPrice(mil),0);
 }
  
 @Test
 public void testSingoloPaninoMenu() throws TakeAwayBillException {
-	TakeAwayBill tb = new TakeAwayBillImpl();
-	MenuItem mi = new MenuItemImpl(ItemType.Panini,"Primavera", 5.0);
-	List<MenuItem> mil = new ArrayList<MenuItem>(); 
-	mil.add(mi);	
+	addElementsToMenu(ItemType.Panini,"Primavera", 5.0, 1);	
 	assertEquals(5,tb.getOrderPrice(mil),0);
 	
 }
  
- 
- 
+@Test
+public void testDuePaniniMenu() throws TakeAwayBillException {
+	addElementsToMenu(ItemType.Panini,"Primavera", 5.0, 2);	
+	assertEquals(10,tb.getOrderPrice(mil),0);
+	
+}
+
 
 }
